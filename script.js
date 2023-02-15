0c9bca87c143cc5b0a13d5af56a87dacefb9a592
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-02-16 00:46:39 +0100

    Add: function getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Test!");
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    switch(choice){
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
