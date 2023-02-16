a8d9b1b7c58f2f5b59dccb9201397ded1dd7b25e
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-02-16 01:02:18 +0100

    Add function playRound()

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ function getComputerChoice(){
     }
 }
 
+// p = player; c = computer
+function playRound(pSelection, cSelection){
+    if(pSelection.toUpperCase() === 'ROCK'){
+        if(cSelection === 'Rock'){
+            return "Draw! Rock ties rock";
+        } else if (cSelection === 'Paper'){
+            return "You Lose! Paper beats rock"
+        } else if(cSelection === 'Scissors'){
+            return "You Win! Rock beats scissors"
+        }
+    }
+
+    if(pSelection.toUpperCase() === 'PAPER'){
+        if(cSelection === 'Rock'){
+            return "You Win! Paper beats rock";
+        } else if (cSelection === 'Paper'){
+            return "Draw! Paper ties rock"
+        } else if(cSelection === 'Scissors'){
+            return "You Lose! Scissors beats paper"
+        }
+    }
+
+    if(pSelection.toUpperCase() === 'SCISSORS'){
+        if(cSelection === 'Rock'){
+            return "You Lose! Rock beats scissors";
+        } else if (cSelection === 'Paper'){
+            return "You Win! Scissors beats paper"
+        } else if(cSelection === 'Scissors'){
+            return "Draw! Scissors tie scissors"
+        }
+    }
+}
+
+console.log(playRound("Rock", getComputerChoice()));
+
+
